88c4a22fbcd7b1d4a62537a7233fbd7fa1bb4d39
segu121 <slowik.sebastian1@gmail.com>
2021-12-07 21:06:15 +0100

    add header, login, setup react

---
+++ src/components/containers/MidContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MidContainerStyle from '../../css/midContainer/midContainer.module.css'
+
+function MidContainer() {
+    return (
+        <div className={MidContainerStyle.midContainer}>
+            <p>Start</p>
+        </div>
+    )
+}
+
+export default MidContainer
---
+++ src/components/functionsComponents/toHeader/LoginFuncComp.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import HeaderStyle from '../../../css/headers/headerBar.module.css';
+
+
+function LoginFuncComp(params) {
+
+
+
+    return (
+        <div className={HeaderStyle.iconContainerHeader}>   
+            <span>Log in</span>
+        </div>
+    );
+    
+
+}
+export default LoginFuncComp;
+
