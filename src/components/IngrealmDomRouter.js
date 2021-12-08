a5af5fad2d55fc5f383a6a84e3735ed3a56c7e29
xtarzyk <61456484+xtarzyk@users.noreply.github.com>
2021-12-08 21:39:31 +0100

    added Admin class & route

--- src/components/IngrealmDomRouter.js
+++ src/components/IngrealmDomRouter.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 import MidContainer from './containers/MidContainer';
 import Login from './classComponents/Login';
+import Admin from './classComponents/Admin';
 
 function IngrealmDomRouter() {
     return (
         <Routes>
             <Route path="/" element={<MidContainer/>} />
             <Route path="/login-page" element={<Login/>} />
+            <Route path="/admin-panel" element={<Admin/>} />
         </Routes>
     );
 }
---
+++ src/components/classComponents/Admin.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Component } from 'react';
+
+class Admin extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <button>Hurrraaa!!!</button>
+            </div>
+        )
+    }
+}
+
+export default Admin
