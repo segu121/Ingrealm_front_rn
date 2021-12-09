cec94049bbd1cf5a5e33368d0f4b280ed5f44850
xtarzyk <61456484+xtarzyk@users.noreply.github.com>
2021-12-09 20:02:33 +0100

    add ingredient table to Admin.js

--- src/components/classComponents/Admin.js
+++ src/components/classComponents/Admin.js
@@ -1,10 +1,39 @@
 import React from 'react'
 import { Component } from 'react';
+import { DataGrid } from '@material-ui/data-grid';
 
 class Admin extends Component {
-    constructor(props) {
-        super(props);
-    }
+
+
+    ingredientsTable() {
+        const columns = [
+            { field : 'id', headerName: 'ID', width: 70},
+            { field : 'name', headerName: 'Name', width: 150},
+            { field : 'rating', headerName: 'Rating', type: 'number', width: 70},
+            { field : 'description', headerName: 'Description', width: 300},
+        ];
+
+        const rows = [
+            { id: 1, name: 'ingredient1', rating: 1.0, description: 'aaaaaa'},
+            { id: 2, name: 'ingredient2', rating: 1.0, description: 'aaaaaa'},
+            { id: 3, name: 'ingredient3', rating: 1.0, description: 'aaaaaa'},
+            { id: 4, name: 'ingredient4', rating: 1.0, description: 'aaaaaa'},
+            { id: 5, name: 'ingredient5', rating: 1.0, description: 'aaaaaa'},
+            { id: 6, name: 'ingredient6', rating: 1.0, description: 'aaaaaa'},
+        ];
+
+        return (
+            <div style={{height: 600, width: '100%'}}>
+                <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    pageSize={50}
+                    rowsPerPageOptions={[50]}
+                    checkboxSelection
+                />
+            </div>
+        );
+    };
 
     addIngredientForm() {
         return (
@@ -18,16 +47,17 @@ class Admin extends Component {
                     </div>
                 </form>
             </div>
-        )
-    }
+        );
+    };
 
     render() {
         return (
             <div>
                 {this.addIngredientForm()}
+                {this.ingredientsTable()}
             </div>
-        )
-    }
+        );
+    };
 }
 
 export default Admin
